f19a18239df761c55ece2cf547daeaa8940083db
Alexandra Shields <alexandrashields@Alexandras-MacBook-Pro.local>
2019-07-11 10:46:26 -0400

    start of js

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+//Comp choices
+var compChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//User choices    
+var userGuesses = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
