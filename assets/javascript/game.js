4467f304dd9523a2b8010941f3638038c9e79484
Alexandra Shields <alexandrashields@Alexandras-MacBook-Pro.local>
2019-07-11 10:51:30 -0400

    links for html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,3 +4,8 @@ var compChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', '
 //User choices    
 var userGuesses = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+//Ready to start
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessedLetters = [];
