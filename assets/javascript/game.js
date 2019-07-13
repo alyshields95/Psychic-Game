f9f3dd861403930e1f28ac848fb5192c1ec26c72
Alexandra Shields <alexandrashields@Alexandras-MacBook-Pro.local>
2019-07-13 13:59:23 -0400

    explanations

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,23 +4,23 @@ var compChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', '
 //User choices    
 var userGuesses = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//Ready to start
+//start
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var guessedLetters = [];
 
-//Key up function
+//Key up
 document.onkeyup = function(event) {
     var userGuesses = event.key;
 
-//Comp picks a random letter
+//Comp picks a letter
 var compGuesses = compChoices[Math.floor(Math.random() * compChoices.length)];
 
-//Logs user and comp choices
+//user and comp choices
     console.log('userGuesses', userGuesses, 'compGuesses', compGuesses); 
 
-//Logging keys 
+
 guessedLetters.push(userGuesses);
 document.getElementById('guessedLetters').innerHTML = "Your guesses so far: " + guessedLetters.join(': ');   
 
@@ -34,10 +34,10 @@ if (userGuesses != compGuesses) {
     document.getElementById('guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
       }
 
-// logs everything to confirm it's working 
+// log
 console.log('wins: ', wins, 'losses: ', losses, 'guesses left: ', guessesLeft, 'guessed letters: ', guessedLetters)      
 
-//Actual reset
+//reset
 var reset = function() {
     guessesLeft = 10;
     guessedLetters = [];
